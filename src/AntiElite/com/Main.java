55beece0fc9b8c55129eb18cdc376d3ed537de06
Nametsego <Nametsegothakadu@gmail.com>
2021-10-12 06:33:41 +0200

    A skateboard is now a part of the vehicle inheritence

--- src/AntiElite/com/Main.java
+++ src/AntiElite/com/Main.java
@@ -4,13 +4,25 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Vehicle vehicle = new Vehicle();
         Bicycle bike = new Bicycle();
         Car car = new Car();
+        Skateboard skateboard = new Skateboard();
+
+
+
+        vehicle.go();
 
         bike.go();
+
+        car.stop();
+
+        skateboard.go();
 
         System.out.println("A bicycle has " + bike.pedals + " pedals");
 
         System.out.println("A car has " + car.windows + " windows");
+
+        System.out.println("A skateboard has " + skateboard.wheels + " wheels and " + skateboard.gripTape + " grip tape for safety");
     }
 }
---
+++ src/AntiElite/com/Skateboard.java
@@ -0,0 +1,7 @@
+package AntiElite.com;
+
+public class Skateboard extends Vehicle {
+    int wheels = 4;
+    int trucks = 2;
+    int gripTape = 1;
+}
